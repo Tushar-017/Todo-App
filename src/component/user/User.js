597e4179dcbf4a4f351d6c98f23ae6433c260e83
Tushar Rajput <tusharo17rajput@gmail.com>
2022-09-24 15:32:08 +0530

    updated

--- src/component/user/User.js
+++ src/component/user/User.js
@@ -1,16 +1,18 @@
-import {ReactComponent as Logo} from "../../images/checked.svg"
+import { ReactComponent as Logo } from "../../images/checked.svg"
 
-import './User.style.css'
+import "./User.style.css"
 
 const User = () => {
   return (
     <div className="User">
       <div className="logo-container">
-        <Logo  width={40} height={40} />
+        <Logo width={40} height={40} />
       </div>
       <div className="info">
-        <p>NO<span className="D">D'</span>ilemma</p>
-        <a href='#' >Logout!</a>
+        <p>
+          NO<span className="D">D'</span>ilemma
+        </p>
+        <a href="#">welcome!</a>
       </div>
     </div>
   )
